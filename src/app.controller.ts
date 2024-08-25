da8c6654fa81a347e3fdb34882e8fc7566a5050b
ritthirat <ritthirat.m@gmail.com>
2024-08-25 22:55:03 +0700

    chore: Add API endpoint for user information retrieval and user creation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -16,5 +16,34 @@ export class AppController {
       username: 'test',
       email: '1@1.com',
     };
+  }
+  @Get('/api/user/info/:id')
+  getthing2(@Param('id') id: string): object {
+    return {
+      username: 'test',
+      email: 'asdasd',
+      id: id,
+    };
+  }
+  @Post('/api/user/info/:id')
+  postthing(@Param('id') id: string): object {
+    const data = [
+      {
+        username: 'test',
+        email: 'asdasd',
+        id: '1',
+      },
+      {
+        username: 'test',
+        email: 'asdasd',
+        id: '2',
+      }
+    ]
+    const user = data.find(user => user.id === id);
+    if (user) {
+      return user;
+    } else {
+      return { error: 'User not found' };
+    }
   }
 }
